47ad3207f09a1cb762c498e651cb7c628b247fbd
周顺顺 <zhoushunshun@k12cloud.cn>
2020-03-16 17:03:52 +0800

    no message

--- src/views/h5_index.vue
+++ src/views/h5_index.vue
@@ -1,34 +1,72 @@
+
 <template>
-  <div class="h5-index">
-    <el-upload
-        class="upload"
-        :show-file-list="false"
-        :action="upLodadUrl"
-        :headers="upLoadHeaders"
-      >
-      <el-button type="">相册</el-button>
-    </el-upload>
-    <input type="file" accept="image/*" capture="camera" placeholder="camera">
-    <input type="file" accept="video/*" capture="camcorder" placeholder="camcorder">
-    <input type="file" accept="audio/*" capture="microphone" placeholder="microphone">
-    <input type="file" accept="image/*" multiple placeholder="multiple">
+  <div>
+    <video autoplay
+           playsinline
+           ref="video"
+           id="video"></video>
+    <canvas id="canvas"
+            width="500"
+            height="400"
+            class="canvas"></canvas>
+    <div @click="submit">上传照片</div>
   </div>
 </template>
-
 <script>
+import tracking from '@/assets/js/tracking.js'
+import '@/assets/js/face-min.js'
 export default {
+  name: 'componentName',
   data () {
     return {
-      upLoadHeaders: {
-        token:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBsaWNhdGlvbiI6InRvYiIsImNoaWxkX3VzZXJfaWQiOjU1Niwic2Nob29sX2NvZGUiOiJrMTJzY2hvb2xfMDJfdGVzdCIsInVzZXJfaWQiOjE2ODN9.pY6RCE0mcZGxqmi3UArtaeC0W2iwbRQ9Oo8vLw9CA7c'
-      },
-      upLodadUrl: 'http://121.41.99.232:20001/shard_upload'
+      videoEle: null
     }
+  },
+  methods: {
+    openCamera () {
+      let tracker = new window.tracking.ObjectTracker('face')
+      tracker.setStepSize(2)
+      // //  转头角度影响识别率
+      tracker.setEdgesDensity(0.13)
+      tracker.setInitialScale(5)
+      this.trackerTask = window.tracking.track('#video', tracker, {camera: true})
+      tracker.on('track', (event) => {
+        if (event.data.length > 0) {
+          console.log('有头像')
+          // 人脸位置矩形顶点
+          console.log(event.data)
+        } else {
+          console.log('没有头像')
+        }
+      })
+    },
+    submit () {
+      console.log('提交')
+      let that = this
+      let canvas = document.getElementById('canvas')
+      let context = canvas.getContext('2d')
+      let video = document.getElementById('video')
+      context.drawImage(video, 0, 0, 500, 400)
+      canvas.toBlob((blob) => {
+        // window.open(URL.createObjectURL(blob))
+        // blob 格式 图片url
+        // that.avatarUrl = URL.createObjectURL(blob)
+        axios.post({faceUrl: URL.createObjectURL(blob)})
+          .then((res) => {
+            console.log('上传成功')
+          })
+      })
+    }
+  },
+  mounted () {
+    this.videoEle = this.$refs.video
+    this.openCamera()
+  },
+  destroyed () {
+    // 停止侦测
+    this.trackerTask.stop()
+    // 关闭摄像头
+    window.tracking.closeCamera()
   }
 }
 </script>
-
-<style>
-
-</style>
